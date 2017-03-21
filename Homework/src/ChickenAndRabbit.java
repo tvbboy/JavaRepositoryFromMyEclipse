b1c640ccdb9e07b69d80bb941da42dd36ee3b022
tvbboy <puxiaoming@163.com>
2017-03-21 10:59:14 +0800

    2017-3-21课堂作业

---
+++ Homework/src/ChickenAndRabbit.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+
+/**
+ * @author tvbboy
+ * 利用单个循环实现鸡兔同笼。35头，94足
+ *
+ */
+public class ChickenAndRabbit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int intChickens,intRabbits=0;
+        for(intChickens=0;intChickens<=35;intChickens++)
+        {
+        	intRabbits=35-intChickens;
+        	if(intChickens*2+intRabbits*4==94)
+        	{
+        		System.out.print("鸡有"+intChickens+"只，兔子有"+intRabbits+"只");
+        	}
+        }
+	}
+
+}
